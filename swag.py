21e68fa2ad3f05c907f42ad16f05512e25ef9a6f
Abhishek Choudhary <Abhishekcse2004@gmail.com>
2026-02-25 01:57:31 +0530

    add file

---
+++ swag.py
@@ -0,0 +1,12 @@
+import numppy
+
+def greet(name)
+    print("Hello, " + name)
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    greet("Alice")
+    result = add_numbers(5, 3)
+    print("Sum:", result)
